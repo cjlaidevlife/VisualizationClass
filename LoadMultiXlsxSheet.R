04907fbbe29b84852a940af63d94e4b23c973e7d
Lai Chun Jing <max716134@gmail.com>
2019-12-13 14:15:15 +0800

    LoadMultiXlsxSheet.R remove use 'library()' import packages

--- LoadMultiXlsxSheet.R
+++ LoadMultiXlsxSheet.R
@@ -1,19 +1,21 @@
-library(openxlsx)
-library(readxl)
-#---
-path <- readline("path:");# Key in your data path.
-findWord <- readline("keyword:");# Key in your dataname or keyword.
-dfName <- readline("data title:");# Key in you want to create sheet data title.
-setwd(path)  #Your data path.
-#---  
+#---Bypass---#
+#install.packages("openxlsx")
+#install.packages("readxl")
+#---Input---#
+path <- readline("Where are you data(path):");
+findWord <- readline("Enter keywords:");
+dfName <- readline("Nickname:");# Name your sheet Nickname from data.
+setwd(path) 
+#---Check data---# 
 foderfile <- list.files(path = path)
 wantdata <- grep(findWord,foderfile) # your dataname or keyword.
-xlsxNumber<- length(excel_sheets(foderfile[wantdata]))
+xlsxNumber<- length(readxl::excel_sheets(foderfile[wantdata]))
 dfName <- paste0(dfName,1:xlsxNumber)
-#---  
+#---Create---#  
 for(i in 1:xlsxNumber){
-  assign(dfName[i],read.xlsx(foderfile[wantdata],sheet = i))
-}# Create sheet data.
-#---
+  assign(dfName[i],openxlsx::read.xlsx(foderfile[wantdata],sheet = i))
+}
+#---Clean---#
 rm(dfName);rm(findWord);rm(foderfile);rm(i);rm(path);rm(wantdata);rm(xlsxNumber);
 cat("\014")# Clear your R console.
+#---End---#
